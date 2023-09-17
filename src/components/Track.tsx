99353dd07a24e8283242d4c3dfcd5258fc5d2a9c
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-09-17 09:21:05 +0100

    Implementing dark mode, enhancing the landing page UI by adding annimation and hover effects

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import '../styles/Track.css';
 import DoHard from './DoHard';
 import desktopImg from "../images/alx-sasa_desktop2(1).png"
+import { Zoom, Slide }from "react-reveal";
 
 const Track:React.FC = () => {
     return (
         <div className='track'>
             <div className='track__width'>
                 <div className='track__content'>
-                    <div className='track__img'>
-                        <img src={desktopImg} alt="desktop representation of alx-sasa showing the project timer" />
-                        <div className='hard'>
-                            <DoHard />
-                        </div> 
-                    </div>
+                    <Slide left>
+                        <div className='track__img'>
+                            <img src={desktopImg} alt="desktop representation of alx-sasa showing the project timer" />
+                            <div className='hard'>
+                                <DoHard />
+                            </div> 
+                        </div>
+                    </Slide>
                     <div className='track__header'>
-                        <h1>TRACK YOUR PROJECTS IN REAL TIME</h1>
+                        <h1><Zoom top cascade>TRACK YOUR PROJECTS IN REAL TIME</Zoom></h1>
                     </div>
                 </div>
             </div>
